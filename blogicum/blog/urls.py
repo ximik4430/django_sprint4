cd668f503e49ad8387c1b9d7f51b405a8e6225f0
Александр Снегирев <ximik4430@gmail.com>
2023-07-08 19:42:05 +0300

    xxx

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -12,43 +12,44 @@ urlpatterns = [
         name='index'
     ),
     path(
-         'posts/<int:pk>/',
-         views.PostDetailView.as_view(),
-         name='post_detail'
+        'posts/<int:pk>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
     ),
     path(
         'posts/create/',
-         views.PostCreateView.as_view(),
-         name='create_post'
+        views.PostCreateView.as_view(),
+        name='create_post'
     ),
     path(
         'posts/<int:pk>/edit/',
-         views.PostUpdateView.as_view(),
-         name='edit_post'
+        views.PostUpdateView.as_view(),
+        name='edit_post'
     ),
     path(
         'posts/<int:pk>/delete/',
-         views.PostDeleteView.as_view(),
-         name='delete_post'
+        views.PostDeleteView.as_view(),
+        name='delete_post'
     ),
     path(
         'category/<slug:category_slug>/',
-         views.category_posts,
-         name='category_posts'
+        views.category_posts,
+        name='category_posts'
     ),
     path(
         'posts/<int:pk>/comment/',
         views.add_comment,
         name='add_comment'
     ),
-    path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
-         views.edit_comment,
-         name='edit_comment'
+    path(
+        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
+        views.edit_comment,
+        name='edit_comment'
     ),
     path(
         'posts/<int:post_id>/delete_comment/<int:comment_id>',
-         views.delete_comment,
-         name='delete_comment'
+        views.delete_comment,
+        name='delete_comment'
     ),
     path(
         'profile/<str:name>/',
@@ -57,7 +58,7 @@ urlpatterns = [
      ),
     path(
         'edit_profile/<slug:name>',
-         views.edit_profile,
-         name='edit_profile'
+        views.edit_profile,
+        name='edit_profile'
      ),
 ]
