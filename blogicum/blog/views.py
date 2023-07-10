3294c8f376c4d15485140289615eb39ae83d9b16
Александр Снегирев <ximik4430@gmail.com>
2023-07-10 20:09:21 +0300

    исправлено

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -40,7 +40,7 @@ def get_paginated_posts(posts, page_number):
     paginator = Paginator(posts, settings.POSTS_PAGE)
     try:
         page_obj = paginator.get_page(page_number)
-    except Exception as e: # pylint: disable=unused-variable
+    except Exception as e:  # pylint: disable=unused-variable
         # Обработка исключения, если номер страницы некорректный
         page_obj = paginator.get_page(1)
     return page_obj
